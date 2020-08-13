07b3ce8f2cf425536311a68d80a485d183aa2fd8
marcizhu <marc.izquierdo.hu@gmail.com>
2020-08-13 11:27:36 +0200

    Added function wrapper test

---
+++ tests/Wrapper.cpp
@@ -0,0 +1,33 @@
+#include "Cache/Cache.h"
+#include "Cache/Wrapper.h"
+#include "Cache/Policy/LRU.h"
+
+#define CATCH_CONFIG_MAIN
+#include "catch2/catch.hpp"
+
+TEST_CASE("Function Wrapper", "[cache][wrapper]")
+{
+	int call_count = 0;
+	auto my_fn = [&call_count](int a, int b) { call_count++; return a + b; };
+
+	auto my_fn_cached = wrap<Policy::LRU>(my_fn, 10U);
+
+	SECTION("The original function is called if the data is not cached")
+	{
+		REQUIRE(call_count == 0);
+		int ret = my_fn_cached(1, 2);
+
+		REQUIRE(ret == 3);
+		CHECK(call_count == 1);
+	}
+
+	SECTION("The original function is NOT called if the data is cached")
+	{
+		REQUIRE(my_fn_cached(2, 2) == 4);
+		REQUIRE(call_count == 1);
+		int ret = my_fn_cached(2, 2);
+
+		REQUIRE(ret == 4);
+		CHECK(call_count == 1);
+	}
+}
